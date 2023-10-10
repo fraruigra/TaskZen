a2ea2c7b22d72d570b4950748142e87805ba8166
Francisco <116636560+fraruigra@users.noreply.github.com>
2023-10-10 18:20:18 +0100

    10/10

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 
 #include<stdio.h>
 #include<conio.h>
-#include <stdlib.h>
+#include<stdlib.h>
 
 #define maxTaskName 25
 #define maxTaskDescription 500
@@ -15,21 +15,42 @@ void modifyTask;
 void deleteTask;
 void showCompleteTask;
 
-int main ()
-{
-    int n, option, counter, day, month, year;
+int main () {
+    int option, counter, day, month, year;
     char taskName[maxTaskName], taskDescription[maxTaskDescription];
-
+    bool menuOption=true;
 
     printf("Hello, I'm Task Zen!\n\n");
 
     do {
+        printf("Main Menu\n\n");
+        printf("1. Add new Task\n");
+        printf("2. Show Active Tasks\n"):
+        printf("3. Modify tasks\n"):    
+        printf("4. Delete tasks\n"):
+        printf("5. Show completed tasks\n"):
+        printf("6. Exit\n"):
+        scanf (%i,option);
 
-    } while
+        switch (option) {
+            case 1:
+                addNewTask;
+            case 2:
+                showActiveTask;
+            case 3:
+                modifyTask;
+            case 4:
+                deleteTask;
+            case 5:
+                showCompleteTask
+            case 6:
+                menuOption=false;
+        }
 
-    //function
-    //function
+    } while (menuOption);
 
-	getch();
+    printf("Thank you! See you soon!"):
+
+	getch();\
 	return 0;
 }
