699d18c2939c511e1558a18de1543aa3e8546ba0
fraruigra <fraruigra@outlook.com>
2023-10-10 12:41:19 +0100

    10/10

---
+++ main.c
@@ -0,0 +1,23 @@
+//Task Zen version 0.1
+//Author: fraruigra || Github: https://github.com/fraruigra || Email: fraruigra@outlook.com
+
+#include<stdio.h>
+#include<conio.h>
+
+#define maxTaskName 25
+#define maxTaskDescription 500
+
+int main ()
+{
+    int counter, day, month, year;
+    char taskName[maxTaskName], taskDescription[maxTaskDescription];
+
+
+    printf("Hello, I'm Task Zen!\n\n");
+
+    //function
+    //function
+
+	getch();
+	return 0;
+}
